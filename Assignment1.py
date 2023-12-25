4c84efa2cbfeb766125b24202dbfcc8281d36dac
Riddesh05 <129084323+Riddesh05@users.noreply.github.com>
2023-12-25 20:38:05 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Q1
+
+# 1 Ans]
+# - Variable name must start with letter or any underscore
+# - Variable name can not start with number
+# - Use spaces around the assignment operator for better readability.
+# - It can contain only aplha numeric characters and underscore
+
+# # Q2
+# 
+
+# 2 Ans]
+# -if we try to declare a variable using a reserved keyword then it will give syntax error.python reserves certtain words as keywords because they are part of the language's syntac and have specific meaning 
+#  for example
+#  if we try to declare a variable using a reserved keyword like 'class':
+#  class ="riddddd"
+#  
+#  we will get a syntax error
+#  SyntaxError: invalid syntax
+# 
+
+# # Q3
+
+# yes we can declare string as variable name like example
+
+# In[2]:
+
+
+v="hello everyone , Myself Ash"
+print(v)
+
+
+# # Q4
+
+# Yes it is possible to  use an underscore a variable name
+# for example:
+
+# In[3]:
+
+
+_a=10
+print(_a)
+
+
+# # Q5
+
+# the dynamic nature of variables in Python:
+
+# In[4]:
+
+
+# Assigning an integer to a variable
+my_variable = 5
+print("Value:", my_variable, "Type:", type(my_variable))
+
+
+# In[ ]:
+
+
+
+
